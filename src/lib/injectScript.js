9da8253be7f2bd6a95bb71626669b36129eef65b
tannerbaum <hoisington.tanner@gmail.com>
2019-03-21 17:53:51 +0100

    overhaul init

--- src/lib/injectScript.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const injectScript = (script) => {
-    const firstScript = document.getElementsByTagName("script")[0];
-    firstScript.parentNode.insertBefore(script, firstScript);
-};
-
-export default injectScript;
---
+++ src/test/test.js
@@ -0,0 +1,27 @@
+// * High level requirements 
+// ! MATOMO
+describe("core client functionality ", () => {
+    // ? One it statement or one for each API function? 
+    // Possibly use a for to examine prototype or properties of our Matomo object and the API itself
+    // should equal 
+    it('should expose all Matomo API functions', () => {
+    
+    });
+    it('should fire corresponding tracking functions in the order they were called', () => {
+    
+    });
+    // * We enforce that async await isnt used
+    // Check if that is how it is implemented in Matomo already 
+    it('does not break the script when it fails (no try/catch needed)', () => {
+    
+    });
+    it('respects the do not track header', () => {
+    
+    });
+
+});
+
+// this is about the cookies thing
+describe("server functionality", () => {
+    
+});
